4a461e0cbd39ab781ef654d7831b658fe952e00a
forer-lika777 <126970640+forer-lika777@users.noreply.github.com>
2025-10-20 09:38:22 +0800

    更新 ChangeMyUITextColorClient.java

--- src/client/java/com/mycompany/change_my_ui_text_color/ChangeMyUITextColorClient.java
+++ src/client/java/com/mycompany/change_my_ui_text_color/ChangeMyUITextColorClient.java
@@ -6,6 +6,5 @@ public class ChangeMyUITextColorClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-  //逆天可以在github app上直接编辑代码文件
 	}
 }
